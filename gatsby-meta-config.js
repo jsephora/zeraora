f39faccd81dd9ae1175669058203a1f7fe5c6e16
jsephora <joey@sephoradesign.com>
2023-04-13 23:07:10 +0900

    updated metadata

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -17,14 +17,14 @@
 
 /** @type {MetaConfig} */
 const metaConfig = {
-  title: "Dev Ed",
-  description: `Ed's Blog`,
-  author: "Ed",
-  siteUrl: "https://gatsby-starter-apple.netlify.app",
+  title: "Zeraora",
+  description: `Joey's Website and Blog`,
+  author: "Joey Sephora",
+  siteUrl: "https://zeraora.gatsbyjs.io/",
   lang: "en",
   utterances: "sungik-choi/gatsby-starter-apple-comment",
   links: {
-    github: "https://github.com/sungik-choi/gatsby-starter-apple",
+    github: "https://github.com/jsephora/zeraora",
   },
   favicon: "src/images/icon.png",
 }
